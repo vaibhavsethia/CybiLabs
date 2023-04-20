71e39c24557343adf3819e868a62a42cc5fadea4
vaibhavsethia <vaibhavsethia1998@gmail.com>
2023-04-21 02:03:01 +0530

    home content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
-import { Layout, Row, Col} from "antd";
+import { Layout, Row, Col, Divider} from "antd";
 import Timeline from "./Components/Timeline/TimeLineElements";
 import Hero from "./Components/Hero";
 import Footer from "./Components/Footer";
 import Contact from './Components/Contact';
-
+import {
+  UserOutlined,
+  LaptopOutlined,
+  HomeOutlined
+} from "@ant-design/icons";
 import "./App.css";
-import PageHeader from "./Utils/PageHeader";
+import { Rotate } from "react-reveal";
 
 const { Content } = Layout;
 
@@ -50,10 +54,80 @@ export class App extends Component {
                   <Col lg={4}>
                     <Timeline />
                   </Col>
-                  <Col lg={20}>
-                    <PageHeader Title="Who are we ?"/>
-                    <PageHeader Title="What do we do ?"/>
-                    <PageHeader Title="Request a consultation"/>
+                  <Col lg={20} className="content">
+                    <div className="tc pa3 mb2 header">
+                      <Rotate top left cascade><span className="green-bold b">Who</span> are we ?</Rotate>
+                    </div>
+
+                    <p className="f3 tc">
+                      Empowering your business with innovative IT solutions.
+                    </p>
+                    <Row gutter={16}>
+                      <Col md={12} sm={24}>
+                        <p className="ml4 mr4 f4">
+                          Looking to turn your innovative ideas into reality? Choose Cybilabs - 
+                          the premier software consultancy company that can bring your visions to life. 
+                          Our team of experienced and skilled professionals work closely with our clients 
+                          from initial consultation to the final product launch, to ensure their vision is brought to 
+                          life with precision and efficiency. Our team stays up-to-date with the latest trends and technologies, 
+                          allowing us to provide cutting-edge solutions tailored to meet the unique needs of each client. 
+                          We understand that every project is different, and we approach each one with a fresh perspective to ensure 
+                          the best possible outcome.
+                        </p>
+                      </Col>
+                      <Col md={12} sm={24} className="f5">
+                        <Row justify="middle" gutter={16}>
+                          <Col>
+                            <UserOutlined className="icons"/>
+                          </Col>
+                          <Col>
+                            <span className="f3 b">Software Consultant</span>
+                          </Col>
+                        </Row>
+                        <p className="pl5">
+                          A software consultant is a professional who provides expert advice and assistance on software development and 
+                          implementation to businesses and organizations. We help our clients identify software solutions that meet their specific 
+                          needs and goals, and provide guidance on best practices and industry standards.
+                        </p>
+
+                        <Row justify="middle" gutter={16}>
+                          <Col>
+                            <UserOutlined className="icons"/>
+                          </Col>
+                          <Col>
+                            <span className="f3 b">Technology Analaysis</span>
+                          </Col>
+                        </Row>
+                        <p className="pl5">
+                          Technology analysis is the process of evaluating and assessing technology to determine its potential benefits, 
+                          limitations, and impact on various stakeholders. We will be analyzing the technical, economic, social, and environmental 
+                          factors surrounding the technology to make informed decisions about your project.
+                        </p>
+                      
+                        <Row justify="middle" gutter={16}>
+                          <Col>
+                            <UserOutlined className="icons"/>
+                          </Col>
+                          <Col>
+                            <span className="f3 b">System Application</span>
+                          </Col>
+                        </Row>
+                        <p className="pl5">
+                          A system application is software designed to perform specific functions within a larger system or environment. 
+                          It will be typically integrated with your other software applications and systems to support the business processes and operations.
+                        </p>    
+                      </Col>
+                    </Row>
+                    
+                    <Divider className="home-divider"/>
+                    
+                    <div className="tc pa3 mb2 header">
+                      <Rotate top left cascade><span className="green-bold b">What</span> do we do ?</Rotate>
+                    </div>
+
+                    <div className="tc pa3 mb2 header">
+                      <Rotate top left cascade><span className="green-bold b">Request</span> a consultation</Rotate>
+                    </div>
                   </Col>
                 </Row>
               </Content>
