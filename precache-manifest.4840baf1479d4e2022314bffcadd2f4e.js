f349a4cf581b6655d0f34692798c6b7f1cb4f6dc
vaibhavsethia <vaibhavsethia1998@gmail.com>
2023-05-17 03:38:21 +0530

    Updates

---
+++ precache-manifest.4840baf1479d4e2022314bffcadd2f4e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31239f28bbce9a480ef5e16344ce41d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d009f298f1293dadcf5",
+    "url": "/static/css/2.6df4c8f8.chunk.css"
+  },
+  {
+    "revision": "408971bc64f8412cbce8",
+    "url": "/static/css/main.a7110c30.chunk.css"
+  },
+  {
+    "revision": "3d009f298f1293dadcf5",
+    "url": "/static/js/2.42560cb6.chunk.js"
+  },
+  {
+    "revision": "f7f6df676dd777668e07d14369e57924",
+    "url": "/static/js/2.42560cb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408971bc64f8412cbce8",
+    "url": "/static/js/main.aab0aa18.chunk.js"
+  },
+  {
+    "revision": "5f836b12f98a53dece18",
+    "url": "/static/js/runtime-main.b1eb175e.js"
+  },
+  {
+    "revision": "0f3ab39f7b6dd5665af00d2caeee1de4",
+    "url": "/static/media/Logo.0f3ab39f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc1090a865dc6c0fd5b5a41663b29ba3.js"
+  "/precache-manifest.4840baf1479d4e2022314bffcadd2f4e.js"
 );
 
 self.addEventListener('message', (event) => {
